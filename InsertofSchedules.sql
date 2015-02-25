ae4083d46049fdb3651c34f65f4251256528ae28
marcgh17 <ghossein.marc@gmail.com>
2015-02-25 18:51:59 +0200

    Fix Script

---
+++ InsertofSchedules.sql
@@ -0,0 +1,19 @@
+
+SET IDENTITY_INSERT [dbo].[Schedule] ON 
+
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(1,'M','Mo','Monday Morning')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(2,'E','Mo','Monday Evening')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(3,'M','Tu','Tuesday Morning')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(4,'E','Tu','Tuesday Evening')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(5,'M','We','Wednesday Morning')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(6,'E','We','Wednesday Evening')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(7,'M','Th','Thursday Morning')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(8,'E','Th','Thursday Evening')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(9,'M','Fr','Friday Morning')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(10,'E','Fr','Friday Evening')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(11,'M','Sa','Saturday Morning')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(12,'E','Sa','Saturday Evening')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(13,'M','Su','Sunday Morning')
+INSERT INTO [dbo].[Schedule]  ([ScheduleID],[ScheduleME],[ScheduleDay],[ScheduleDescription]) VALUES(14,'E','Su','Sunday Evening')
+
+SET IDENTITY_INSERT [dbo].[Schedule] OFF
